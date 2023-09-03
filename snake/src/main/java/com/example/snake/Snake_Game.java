c528d0b39a65b53df507094201b0b6ec3a53ef19
Siddh <siddhkothari99@gmail.com>
2023-09-03 21:54:03 +0530

    First Commit
    Uploading All files

---
+++ snake/src/main/java/com/example/snake/Snake_Game.java
@@ -0,0 +1,10 @@
+package com.example.snake;
+
+import java.util.Scanner;
+
+public class Snake_Game {
+    public static void main(String[] args) {
+
+        new GameFrame(); //call GameFrame class
+    }
+}
